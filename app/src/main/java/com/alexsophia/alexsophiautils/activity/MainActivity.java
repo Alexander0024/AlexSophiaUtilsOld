16ab5b55eb75ed1bea583af6d2cd78434024c81a
Alexander0024 <lizhe0024@126.com>
2016-02-26 17:19:53 +0800

    Add fragment activity.

--- app/src/main/java/com/alexsophia/alexsophiautils/activity/MainActivity.java
+++ app/src/main/java/com/alexsophia/alexsophiautils/activity/MainActivity.java
@@ -1,16 +1,47 @@
 package com.alexsophia.alexsophiautils.activity;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.alexsophia.alexsophiautils.util.generator.CountingGenerator;
+import com.alexsophia.alexsophiautils.R;
 
 /**
- * Main activity for test functions
+ * Main activity
  */
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
+	private Button fragment_activity;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.main);
 		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void findViewById() {
+		fragment_activity = (Button) findViewById(R.id.fragment_activity);
+	}
+
+	@Override
+	public void initView() {
+
+	}
+
+	@Override
+	public void setListener() {
+		fragment_activity.setOnClickListener(listener);
+	}
+
+	@Override
+	public void clickView(View v) {
+		switch (v.getId()) {
+			case R.id.fragment_activity:
+				startActivity(new Intent(this, FragmentActivity.class));
+				break;
+			default:
+				super.clickView(v);
+		}
 	}
 }
