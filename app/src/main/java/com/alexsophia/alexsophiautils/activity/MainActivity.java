c3494e5c136d4000424a12a0d2d30675f287a422
Alexander0024 <lizhe0024@126.com>
2016-03-10 21:28:16 +0800

    Add generator and textview builder.

--- app/src/main/java/com/alexsophia/alexsophiautils/activity/MainActivity.java
+++ app/src/main/java/com/alexsophia/alexsophiautils/activity/MainActivity.java
@@ -2,15 +2,16 @@ package com.alexsophia.alexsophiautils.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import com.alexsophia.alexsophiautils.util.generator.CountingGenerator;
+import com.alexsophia.alexsophiautils.util.TextViewBuilder;
 
-/**
- * Main activity for test functions
- */
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		TextView tv = null;
+		tv.setText(new TextViewBuilder().setText("asdfasdfas")
+				.setTextSize(24, true, 0, 4).build());
 	}
 }
